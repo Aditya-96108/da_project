72ace8668eaf78d917c47c264b0d54c8759d1127
Aditya-96108 <115214643+Aditya-96108@users.noreply.github.com>
2024-07-02 19:47:27 +0530

    Update host.py

--- host.py
+++ host.py
@@ -1,70 +1,36 @@
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import pickle
 import streamlit as st
 
-
-# loading the saved model
 loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
-
-# creating a function for Prediction
-
 def diabetes_prediction(input_data):
-    
-
-    # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
-
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
     prediction = loaded_model.predict(input_data_reshaped)
-    return prediction[0]
+    if prediction[0] == 0:
+        return 'The person is not diabetic'
+    else:
+        return 'The person is diabetic'
 
-    
-  
-    
-  
 def main():
-    
-    
-    # giving a title
     st.title('Diabetes Prediction Web App')
-    
-    
-    # getting the input data from the user
-    
-    
-    Pregnancies = st.text_input('Number of Pregnancies')
-    Glucose = st.text_input('Glucose Level')
-    BloodPressure = st.text_input('Blood Pressure value')
-    SkinThickness = st.text_input('Skin Thickness value')
-    Insulin = st.text_input('Insulin Level')
-    BMI = st.text_input('BMI value')
-    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    Age = st.text_input('Age of the Person')
-    
-    
-    # code for Prediction
 
-    
-    # creating a button for Prediction
-    
+    Pregnancies = st.text_input('Number of Pregnancies', '0')
+    Glucose = st.text_input('Glucose Level', '85')
+    BloodPressure = st.text_input('Blood Pressure value', '80')
+    SkinThickness = st.text_input('Skin Thickness value', '20')
+    Insulin = st.text_input('Insulin Level', '85')
+    BMI = st.text_input('BMI value', '22.0')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value', '0.5')
+    Age = st.text_input('Age of the Person', '30')
+
+    diagnosis = ''
+
     if st.button('Diabetes Test Result'):
         diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-        if (diagnosis== 0):
-            st.success('The person is not diabetic')
-        else:
-            st.error('The person is diabetic')
-        
-        
-    
-    
-    
-    
-    
-    
+
+    st.success(diagnosis)
+
 if __name__ == '__main__':
     main()
